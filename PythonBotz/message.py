cf6a5c60f1e7257137878986f6e8e800a946a6f3
KX3M <163962831+KX3M@users.noreply.github.com>
2025-08-02 17:49:23 +0530

    Create message.py

---
+++ PythonBotz/message.py
@@ -0,0 +1,15 @@
+CHANNEL_ID = '@Drsudo'
+DEV_USERNAME = '@metaui'
+
+# UPI Premium Message
+message_content =  '''👋 <b>Hey {first}\n
+🎖️ <u>Available Plans</u>:</b>\n
+<blockquote expandable><i>● 30 rs For 7 Days Prime Membership\n
+● 110 rs For 1 Month Prime Membership\n
+● 299 rs For 3 Months Prime Membership\n
+● 550 rs For 6 Months Prime Membership\n
+● 999 rs For 1 Year Prime Membership</i></blockquote>\n
+💵 UPI ID - <code>trustable@upi</code>
+<b>(Tap to copy UPI Id)</b>\n\n
+♻️ <b>If payment is not getting sent on above given QR code then inform Admin, He will give you new QR code</b>\n
+‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ'''
